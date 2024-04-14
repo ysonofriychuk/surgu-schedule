bcc1e2c5c246a0ade0e4ce267740039c96fadf8a
Xalatte <timursvistunov2@gmail.com>
2024-04-14 18:31:50 +0500

    что-то сделал вроде как

--- internal/bot/routers/set_group.py
+++ internal/bot/routers/set_group.py
@@ -2,11 +2,16 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-router = Router()
+from internal.schedule.schedule import Schedule
 
+
+router = Router()
+schedule = Schedule({})
+
+with open('internal/bot/assets/message/msg_set-group.txt', 'r', encoding="utf-8") as file:
+    msg_sg = file.read()
 
 @router.message(Command("set-group"))
 async def cmd_start(message: Message):
-    # TODO SURGU-010
-    pass
-
+    if len(message.text.split()) != 2:
+        await message.answer(msg_sg)
