deebad430f72307d6272aba3fe234c38f9b39e54
dozddd <polinahandogina86@gmail.com>
2024-04-21 12:20:57 +0500

    SURGU-db db

--- internal/db/entity/user.py
+++ internal/db/entity/user.py
@@ -5,3 +5,17 @@
 # created_at
 
 # Добавление нового пользователя / обновление данных текущего
+import datetime
+
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+# Модель пользователя
+class User(Base):
+    __tablename__ = "User"
+
+    user_id = Column(Integer, primary_key=True)
+    group = Column(String(100), default=0)
+    created_at = Column(DateTime(), default=datetime.now)
