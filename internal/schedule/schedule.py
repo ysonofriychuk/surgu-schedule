84d8c6cd9b4e55fa9a08016972d97d3939e1be91
Gogolshaunch <almira08_kz@mail.ru>
2024-04-28 15:41:01 +0500

    Update shedule

--- internal/schedule/schedule.py
+++ internal/schedule/schedule.py
@@ -19,7 +19,7 @@ class Lesson:
 @dataclass
 class ScheduleClass:
     groupNumber: str
-    currentDate = str
+    currentDate: str
     dayWeek: int
     weekType: int
     date: str
@@ -35,26 +35,55 @@ class Schedule:
         return group_number.lower() in self.data
 
     def get_schedule(self, group_number: str, date):
-        # Структуру ниже нужно возвращать в виде data класса
-        # LOOK https://habr.com/ru/articles/415829/
         # Расписание может отличаться
         if not self.group_exist(group_number):
             return None
 
-        date_strings = date[:10]
-        date_times = datetime.strptime(date_strings, "%d-%m-%Y")
+        date_times = datetime.strptime(date, "%d-%m-%Y-%H-%M")
         dayWeek = date_times.weekday()
+
+        week_number = date_times.isocalendar()[1]
+        weekType = 0
+        if week_number % 2 == 0:
+            weekType = 1
+
         lesson = []
 
-        for i in self.data[group_number][day[dayWeek]]:
-            les = Lesson('', i, self.data[group_number][day[dayWeek]][i], )
-            print(self.data[group_number][day[dayWeek]][i])
+        times = date_times.time()
+        num = self.data[group_number][day[dayWeek]].keys()
 
+        for i in range(int(list(num)[-1])):
+            try:
+                a = self.data[group_number][day[dayWeek]][str(i+1)]
+                time = get_lesson_time(a, i+1)
 
+                time1 = datetime.strptime(time.from1, "%H-%M")
+                time2 = datetime.strptime(time.to, "%H-%M")
+                cur = False
+                if time1.time() <= times <= time2.time():
+                    cur = True
+                c = Lesson('lesson', i+1, a, str(time), cur)
+                lesson.append(c)
+            except:
+                time = get_lesson_time('', i + 1)
+                time1 = datetime.strptime(time.from1, "%H-%M")
+                time2 = datetime.strptime(time.to, "%H-%M")
+                cur = False
+                if time1.time() <= times <= time2.time():
+                    cur = True
+                a = Lesson('window', i+1, '', str(time), cur)
+                lesson.append(a)
+
+        schedule = ScheduleClass(groupNumber=group_number, currentDate=str(datetime.now()), dayWeek=dayWeek, weekType=weekType,
+                                 date=date, lessons=lesson)
+        return schedule
+
+"""
 if __name__ == "__main__":
     with open('schedule.json', "r", encoding='utf-8') as file:
         configuration = json.load(file)
 
     sh = Schedule(configuration)
 
-    sh.get_schedule("304-21", '22-04-2024-11-30')
+    sh.get_schedule("303-31м", '25-04-2024-11-30')
+"""
