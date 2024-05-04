679d07aa83931d181f789874145437cd5e797885
xalatte <xam.79@inbox.ru>
2024-05-04 21:20:45 +0500

    напакостил чуть-чуть добавил красивого котика на окончание добавление данных в бд. Чут-чут почистил код. Поставьте 5 умоляю!

--- internal/bot/routers/setgroup.py
+++ internal/bot/routers/setgroup.py
@@ -7,11 +7,17 @@ from aiogram.types import Message, FSInputFile
 from internal.db.db import set_group
 from internal.schedule.schedule import Schedule
 
-router = Router()
 
-with open('internal/bot/assets/message/msg_setgroup.txt', 'r', encoding="utf-8") as file:
-    msg_sg = file.read()
+router = Router()
+schedule = Schedule({})
+
+with open('internal/bot/assets/message/msg_setgroup_bad.txt', 'r', encoding="utf-8") as file:
+    msg_sgbad = file.read()
 photo_baduser = FSInputFile("internal/bot/assets/images/setgroup-bad.png")
+
+with open('internal/bot/assets/message/msg_setgroup_good.txt', 'r', encoding="utf-8") as file:
+    msg_sggood = file.read()
+photo_gooduser = FSInputFile("internal/bot/assets/images/setgroup-good.jpg")
 
 with open('internal/schedule/schedule.json', "r", encoding='utf-8') as file:
     configuration = json.load(file)
@@ -22,12 +28,11 @@ schedule = Schedule(configuration)
 @router.message(Command("setgroup"))
 async def cmd_start(message: Message):
     if len(message.text.split()) != 2:
-        await message.answer_photo(photo=photo_baduser, caption=msg_sg.format(name=message.from_user.full_name))
+        await message.answer_photo(photo=photo_baduser, caption=msg_sgbad.format(name=message.from_user.full_name))
         return
     if not schedule.group_exist(message.text.split()[-1]):
-        await message.answer_photo(photo=photo_baduser, caption=msg_sg.format(name=message.from_user.full_name))
+        print(message.from_user.id, message.text.split()[-1])
+        await message.answer_photo(photo=photo_baduser, caption=msg_sgbad.format(name=message.from_user.full_name))
         return
     set_group(message.from_user.id, message.text.split()[-1])
-    print(message.from_user.id, message.text.split()[-1])
-
-
+    await message.answer_photo(photo=photo_gooduser, caption=msg_sggood.format(name=message.from_user.full_name))
