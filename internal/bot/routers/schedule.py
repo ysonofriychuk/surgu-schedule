c8a9b4ef3488d67aff045dadc3a7e271a46e525c
programdeveloper876 <herniablin@gmail.com>
2024-05-19 17:51:40 +0500

    изменена ссылка на стр расписания

--- internal/bot/routers/schedule.py
+++ internal/bot/routers/schedule.py
@@ -1,3 +1,5 @@
+import datetime
+
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message, FSInputFile, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
@@ -20,37 +22,49 @@ with open('internal/bot/assets/message/msg_schedule_baduser.txt', 'r', encoding=
 photo_schedulebad = FSInputFile("internal/bot/assets/images/schedule-bad.jpg")
 
 
+def get_date_by_week_day(week_day):
+    date = datetime.datetime.now()
+    if week_day:
+        if date.day <= week_day:
+            date += datetime.timedelta(days=week_day)
+        else:
+            date += datetime.timedelta(days=7 - date.weekday() + week_day)
+
+    return date.strftime("%d-%m-%Y-%H-%M")
+
+
 @router.message(Command("schedule"))
 async def cmd_start(message: Message):
-    if not get_group(message.from_user.id):
-        await message.answer_photo(photo=photo_schedulebad,
-                                   caption=msg_schedulebad.format(name=message.from_user.full_name))
-        return
+    user_id = message.from_user.id
+    group_user = get_group(user_id)
 
-    # TODO: убрать посредника перенеся код из internal/api/routers/user_group.py
-    # TODO: URL = f"https://{host}/schedule?group={}&date={}
-    user_id = str(message.from_user.id)
+    if not group_user:
+        await message.answer_photo(
+            photo=photo_schedulebad,
+            caption=msg_schedulebad.format(name=message.from_user.full_name)
+        )
+        return
 
     btn = InlineKeyboardMarkup(
         inline_keyboard=[
            [InlineKeyboardButton(text='Понедельник',
                                  web_app=WebAppInfo(
-                                     url=f"https://{host}/user?user_id={user_id}&week_day={0}"))],
+                                     url=f"https://{host}/schedule?group={group_user}&date={get_date_by_week_day(0)}"))],
            [InlineKeyboardButton(text="Вторник",
                                  web_app=WebAppInfo(
-                                     url=f"https://{host}/user?user_id={user_id}&week_day={1}"))],
+                                     url=f"https://{host}/schedule?group={group_user}&date={get_date_by_week_day(1)}"))],
            [InlineKeyboardButton(text="Среда",
                                  web_app=WebAppInfo(
-                                     url=f"https://{host}/user?user_id={user_id}&week_day={2}"))],
+                                     url=f"https://{host}/schedule?group={group_user}&date={get_date_by_week_day(2)}"))],
            [InlineKeyboardButton(text="Четверг",
                                  web_app=WebAppInfo(
-                                     url=f"https://{host}/user?user_id={user_id}&week_day={3}"))],
+                                     url=f"https://{host}/schedule?group={group_user}&date={get_date_by_week_day(3)}"))],
            [InlineKeyboardButton(text="Пятница",
                                  web_app=WebAppInfo(
-                                     url=f"https://{host}/user?user_id={user_id}&week_day={4}"))],
+                                     url=f"https://{host}/schedule?group={group_user}&date={get_date_by_week_day(4)}"))],
            [InlineKeyboardButton(text="Суббота",
                                  web_app=WebAppInfo(
-                                     url=f"https://{host}/user?user_id={user_id}&week_day={5}"))],
+                                     url=f"https://{host}/schedule?group={group_user}&date={get_date_by_week_day(5)}"))],
         ])
 
     await message.answer_photo(
